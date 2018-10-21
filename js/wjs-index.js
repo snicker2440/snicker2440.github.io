163dc781969f0c285cb77ffddcbcb50882ecd7eb
snicker2440 <42404091+snicker2440@users.noreply.github.com>
2018-10-21 14:41:21 +0800

    Add files via upload

--- js/wjs-index.js
+++ js/wjs-index.js
@@ -2,9 +2,6 @@
  * Created by itcast.
  */
 $(function(){
-    /*初始化工具提示*/
-    $('[data-toggle="tooltip"]').tooltip();
-
     /*获取当前所有item*/
     var items=$(".carousel-inner .item");
     /*监听屏幕的大小改变*/
@@ -20,7 +17,8 @@ $(function(){
                 var imgSrc=item.data("largeImage");
                 console.log(imgSrc);
                 /*添加非移动端的子元素*/
-                item.html($('<a href="javascript:;" class="pcImg"></a>').css("backgroundImage","url('"+imgSrc+"')"));
+                // item.html($('<a href="javascript:;" class="pcImg"></a>').css("backgroundImage","url('"+imgSrc+"')"));
+                item.html($('<a href="javascript:;" class="pcImg" style="background-image: url('+imgSrc+');"></a>'))
             });
         }
         else{
@@ -32,13 +30,31 @@ $(function(){
         }
     }).trigger("resize");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /*添加移动端的滑动操作*/
     var startX,endX;
     var carousel_inner=$(".carousel-inner")[0];
-
     /*获取当前轮播图*/
     var carousel=$(".carousel");
-
     carousel_inner.addEventListener("touchstart",function(e){
         startX= e.targetTouches[0].clientX;
     });
@@ -53,38 +69,4 @@ $(function(){
             carousel.carousel('next');
         }
     });
-
-
-    /*计算产品块导航项的原始宽度*/
-    var ul=$(".wjs_product .nav-tabs");
-    var lis=ul.find("li");
-    var totalWidth=0;//总宽度
-
-
-    lis.each(function(index,ele){
-
-        totalWidth=totalWidth+$(ele).innerWidth();
-        console.log($(ele).innerWidth());
-        /*获取宽度的方法的说明：
-        * width():它只能得到当前元素的内容的宽度
-        * innerWidth():它能获取当前元素的内容的宽度+padding
-        * outerWidth():获取当前元素的内容的宽度+padding+border
-        * outerWidth(true):获取元素的内容的宽度+padding+border+margin*/
-    })
-
-    ul.width(totalWidth);
-
-
-
-
-    /*使用插件实现导航条的滑动操作*/
-    var myScroll = new IScroll('.tabs_parent',{
-        /*设置水平滑动，不允许垂直滑动*/
-        scrollX: true, 
-        scrollY: false
-    });
-
-
-
-
 });
